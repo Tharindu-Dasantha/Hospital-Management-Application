0579a95379dfaeaf9394ce9ec260e90d5bbbfccf
Dasantha Edirisinghe <114149683+Tharindu-Dasantha@users.noreply.github.com>
2024-08-12 18:00:27 +0530

    finalized the backend and started coding the front end

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const patientsRouter = require("./routes/patient");
 const doctorsRouter = require("./routes/doctor");
-const appointmentsRouter = require("./routes/appointment");
+const appointmentsRouter = require("./routes/appointments");
 
 require("dotenv").config();
 
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect("mongodb+srv://tharinduEdirisinghe:WMNSvA0lDa50hoc4@hospitalmanagement.kkpx4.mongodb.net/?retryWrites=true&w=majority&appName=Hospitalmanagement", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
